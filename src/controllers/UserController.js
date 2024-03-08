eddbb2cebc6e27ccf75cb5ee75be60f9d7dbcf62
GabrielCL0803 <gabriel.51905@aluno.pr.senac.br>
2024-03-08 10:55:31 -0300

    Inserção e criação de dados e comandos ao arquivo UserController

---
+++ src/controllers/UserController.js
@@ -0,0 +1,44 @@
+const knex = require("../database/knex")
+
+class UserController{
+    async createUser(req,res){
+        const {name,email,telefone,created_at} = req.body
+        
+        const user = await knex("users").insert({name, email, password, telefone, created_at})
+        return res.status(201).json("Usuário cadastrado com sucesso")
+    }
+
+    async listUser(req,res){
+        const users = await knex("users")
+        
+        return res.status(200).json(users)
+    }
+
+    async listUserById(req,res){
+        const {user_id} = req.params
+
+        const [user] = await knex("users").where({id:user_id})
+
+        return res.status(200).json(user)
+    }
+
+
+    async updateUser(req,res){
+        const {user_id} = req.params
+        const {name, email, telefone} = req.body
+
+        await knex("users").where({id:user_id}).update({name,email,telefone})
+
+        return res.status(200).json("Usuário atualizado com sucesso!")
+    }
+
+    async deleteUser(req,res){
+        const {user_id} = req.params
+
+        await knex("users").where({id:user_id}).delete()
+
+        return res.status(200).json("Registro deletado com sucesso!")
+    }
+
+}
+module.exports = UserController
