305604d68e30f6d58d2461807281a02fb9d99cd5
GabrielCL0803 <gabriel.51905@aluno.pr.senac.br>
2024-03-08 11:01:51 -0300

    Modificação de inicialização para o terminal

--- src/database/knex/migrations/20240308131225_createBooks.js
+++ src/database/knex/migrations/20240308131225_createBooks.js
@@ -1,15 +1,9 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
+
+exports.up = (knex) => {
   
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
+
+exports.down = (knex) => {
   
 };
---
+++ src/routes/user.routes.js
@@ -0,0 +1,17 @@
+const {Router} = require("express")
+const UserController = require("../controllers/UserController")
+
+const CheckUserExist = require("../middlewares/CheckUserExist")
+
+
+const userRoutes = Router()
+const userController = new UserController()
+
+userRoutes.post("/user", userController.createUser)
+userRoutes.get("/user", userController.listUser)
+userRoutes.get("/user/:user_id", userController.listUserById)
+
+userRoutes.put("/user/:user_id", userController.updateUser)
+userRoutes.delete("/user/:user_id", userController.deleteUser)
+
+module.exports = userRoutes
