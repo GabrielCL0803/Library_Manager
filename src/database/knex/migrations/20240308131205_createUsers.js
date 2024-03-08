876c939399e647f1d6149b40bac59bd5f664db1f
GabrielCL0803 <gabriel.51905@aluno.pr.senac.br>
2024-03-08 10:16:38 -0300

    inserção de dados as tabelas do banco de dados

--- src/database/knex/migrations/20240308131205_createUsers.js
+++ src/database/knex/migrations/20240308131205_createUsers.js
@@ -1,15 +1,15 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
-  
+
+exports.up = (knex) => {
+    return knex.schema.createTable("users", (table) => {
+        table.increments('id').primary();
+        table.string("name").notNullable();
+        table.string("email").notNullable();
+        table.string("telefone").notNullable();
+        table.timestamp("created_at").defaultTo(knex.fn.now())
+      })
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
+
+exports.down = (knex) => {
+  return knex.schema.dropTableIfExist("users")
 };
