c97dda5994249082e2349119e0565110ced634c2
GabrielCL0803 <gabriel.51905@aluno.pr.senac.br>
2024-03-11 08:19:51 -0300

    Inserção de rota no index de rota

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const {Router} = require("express")
 const userRoutes = require("./user.routes")
+const bookRoutes = require("./book.routes")
 
 const routes = Router()
 
 routes.use("/", userRoutes)
+routes.use("/", bookRoutes)
 
 module.exports = routes
