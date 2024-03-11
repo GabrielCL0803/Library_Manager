7c6a4c731de4f609377117e83bf6deb271fd6e45
GabrielCL0803 <gabriel.51905@aluno.pr.senac.br>
2024-03-11 09:23:03 -0300

    Criação e configuração de middlewares

--- src/routes/book.routes.js
+++ src/routes/book.routes.js
@@ -1,12 +1,14 @@
 const {Router} = require("express")
 const BooksController = require("../controllers/BooksController")
+const checkUserExists = require("../middlewares/CheckUserExist")
+const checkBookExist = require("../middlewares/CheckBookExist")
 
 const bookRoutes = Router()
 const bookController = new BooksController()
 
-bookRoutes.post("/books/:user_id", bookController.createBook)
+bookRoutes.post("/books/:user_id",checkUserExists, bookController.createBook)
 bookRoutes.get("/books", bookController.listBook)
-bookRoutes.get("/books/:id", bookController.listBookById)
-bookRoutes.delete("/books/:id", bookController.deleteBook)
+bookRoutes.get("/books/:id",checkBookExist, bookController.listBookById)
+bookRoutes.delete("/books/:id",checkBookExist, bookController.deleteBook)
 
 module.exports = bookRoutes
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,14 +1,15 @@
 const {Router} = require("express")
 const UserController = require("../controllers/UserController")
+const checkUserExists = require("../middlewares/CheckUserExist")
 
 const userRoutes = Router()
 const userController = new UserController()
 
 userRoutes.post("/user", userController.createUser)
 userRoutes.get("/user", userController.listUser)
-userRoutes.get("/user/:user_id", userController.listUserById)
+userRoutes.get("/user/:user_id",checkUserExists, userController.listUserById)
 
-userRoutes.put("/user/:user_id", userController.updateUser)
-userRoutes.delete("/user/:user_id", userController.deleteUser)
+userRoutes.put("/user/:user_id",checkUserExists, userController.updateUser)
+userRoutes.delete("/user/:user_id",checkUserExists, userController.deleteUser)
 
 module.exports = userRoutes
