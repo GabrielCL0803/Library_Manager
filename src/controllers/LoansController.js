32eb36f6315d2d88ca4601d87a3fcf7d47f988ac
GabrielCL0803 <gabriel.51905@aluno.pr.senac.br>
2024-03-12 11:02:39 -0300

    Configuração e criação rotas do Loans

--- src/controllers/LoansController.js
+++ src/controllers/LoansController.js
@@ -24,11 +24,22 @@ class LoansController{
     }
 
     async listBorrowedBook(req, res) {
-        
+        const {user_id} = req.params
+
+        const loans = await knex("loans")
+        .where({user_id})
+        .innerJoin("books", "books.idBook", "loans.book_id")
+        .select("books.author", "books.title", "books.category", "books.pages") 
+
+        return res.status(200).json(loans)
     }
 
     async totalBorrowedBooks(req, res) {
+       const {user_id} = req.params
        
+       const [total] = await knex("loans").where({user_id}).count({books: "book_id"})
+       
+       return res.status(200).json(total)
     }
 
 
--- src/routes/loans.routes.js
+++ src/routes/loans.routes.js
@@ -5,5 +5,7 @@ const loanRoutes = Router()
 const loanController = new LoansController()
 
 loanRoutes.post("/:user_id/:book_id", loanController.borrowBooks)
+loanRoutes.get("/:user_id", loanController.listBorrowedBook)
+loanRoutes.get("/total/:user_id", loanController.totalBorrowedBooks)
 
 module.exports = loanRoutes
