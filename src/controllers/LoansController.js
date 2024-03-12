f56c37f9eeaa68a2975b75bf149a63e740e95ece
GabrielCL0803 <gabriel.51905@aluno.pr.senac.br>
2024-03-12 11:25:00 -0300

    Configuração do Loans

--- src/controllers/LoansController.js
+++ src/controllers/LoansController.js
@@ -36,7 +36,7 @@ class LoansController{
 
     async totalBorrowedBooks(req, res) {
        const {user_id} = req.params
-       
+
        const [total] = await knex("loans").where({user_id}).count({books: "book_id"})
        
        return res.status(200).json(total)
@@ -44,7 +44,28 @@ class LoansController{
 
 
     async returnBorrowedBooks(req,res){
-       
+        const {user_id, book_id} = req.params
+
+        const book = await knex("books").where({idBook: book_id}).first()
+        const user = await knex("users").where({idUser: user_id}).first()
+
+        if(!book){
+            return res.status(400).json("Livro não encontrado!")
+        }
+
+        if(!user){
+            return res.status(400).json("Usuário não encontrado!")
+        }
+
+        const [loan] = await knex("loans").where({user_id})
+        const bookId = loan.book_id
+
+        if(bookId == book_id){
+            await knex("books").where({idBook: book_id}).update({isAvailable: true})
+            return res.status(200).json("Livro devolvido com sucesso!")
+        }
+
+        return res.status(200).json("Operação não realizada!")
     }
 
 }
--- src/routes/loans.routes.js
+++ src/routes/loans.routes.js
@@ -7,5 +7,6 @@ const loanController = new LoansController()
 loanRoutes.post("/:user_id/:book_id", loanController.borrowBooks)
 loanRoutes.get("/:user_id", loanController.listBorrowedBook)
 loanRoutes.get("/total/:user_id", loanController.totalBorrowedBooks)
+loanRoutes.patch("/devo/:user_id/:book_id", loanController.returnBorrowedBooks)
 
 module.exports = loanRoutes
