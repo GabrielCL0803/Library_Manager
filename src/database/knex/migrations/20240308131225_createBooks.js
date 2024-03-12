e9996da14890b6201e767b9b0050e78523b14ef8
GabrielCL0803 <gabriel.51905@aluno.pr.senac.br>
2024-03-12 08:29:58 -0300

    Ajustes

--- src/database/knex/migrations/20240308131225_createBooks.js
+++ src/database/knex/migrations/20240308131225_createBooks.js
@@ -13,18 +13,3 @@ exports.up = (knex) => {
 exports.down = (knex) => {
   return knex.schema.dropTableIfExist("books")
 };
-/* exports.up = (knex) => {
-    return knex.schema.createTable("users", (table) => {
-        table.increments("idUser").primary();
-        table.string("name").notNullable();
-        table.string("email").notNullable();
-        table.string("telefone").notNullable();
-        table.timestamp("created_at").defaultTo(knex.fn.now());
-        table.integer("book_id").unsigned().index().references("idBook").inTable("books")
-      })
-};
-
-
-exports.down = (knex) => {
-  return knex.schema.dropTableIfExist("users")
-}; */
